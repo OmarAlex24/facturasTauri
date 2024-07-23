d30e1bd9a438c2e2e6384fc25c586270dab395da
OmarAlex24 <artcate208@gmail.com>
2024-07-22 18:19:21 -0600

    chore: Refactor App component and add routing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { useFileProcessing } from './hooks/useFileProcessing';
-import { useOpenExcelFile } from './hooks/useOpenExcelFile';
-import { FileProcessor } from './components/FileProcessor';
-import { ExcelDisplay } from './components/ExcelDisplay';
-import { useDocDir } from './hooks/useDocDir';
+import XmlReader from './pages/XmlReader';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
 
-const App: React.FC = () => {
-  const docDir = useDocDir();
-  const { excelPath, processFiles } = useFileProcessing(docDir);
-  const { openExcelFile } = useOpenExcelFile(excelPath);
-
+const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-green-100 flex items-center justify-center p-4">
-      <Card className="w-full max-w-md shadow-lg">
-        <CardHeader className="text-center">
-          <CardTitle className="text-2xl font-bold text-blue-700">Factura XML Processor</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <FileProcessor processFiles={processFiles} />
-
-          {excelPath && (
-            <ExcelDisplay openExcelFile={openExcelFile} />
-          )}
-        </CardContent>
-      </Card>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/xmlReader" element={<XmlReader />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Layout>
+    </Router>
 
   );
 };
